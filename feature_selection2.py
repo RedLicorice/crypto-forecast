fd16340e423d827311debbbd7ea5f5d2ca153ce5
Giuseppe <p3pp1n0@gmail.com>
2020-07-16 17:12:46 +0200

    changed cv and split parameters

--- feature_selection2.py
+++ feature_selection2.py
@@ -12,53 +12,6 @@ from sklearn.svm import SVC
 import numpy as np
 import json
 
-def rfecv_rank(X, y, columns):
-    scaler = MinMaxScaler()
-    # Features need to be scaled in order to reduce the problem's complexity
-    # Otherwise, SVC training time will be ages
-    X = scaler.fit_transform(X)
-    estimator = SVC(kernel='linear')
-    selector = RFECV(estimator, step=1, cv=3, verbose=1)
-    selector = selector.fit(X, y)
-    # selector ranking to column:rank pairs
-    rank = {columns[i]:s for i, s in enumerate(selector.ranking_)}
-    return {
-        # sort rank by values
-        'rank': {str(k): int(v) for k, v in sorted(rank.items(), key=lambda item: item[1])},
-        # pick selected features names
-        'support': [columns[i] for i, s in enumerate(selector.support_) if s]
-    }
-
-def genetic_select(X, y, columns):
-    scaler = MinMaxScaler()
-    # Features need to be scaled in order to reduce the problem's complexity
-    # otherwise, LogisticRegression will fail to converge!
-    X = scaler.fit_transform(X)
-    estimator = LogisticRegression(solver="liblinear", multi_class="ovr")
-    selector = GeneticSelectionCV(estimator,
-                                      cv=3,
-                                      verbose=1,
-                                      scoring="accuracy",
-                                      max_features=min(X.shape[1], 30),
-                                      n_population=50,
-                                      crossover_proba=0.5,
-                                      mutation_proba=0.2,
-                                      n_generations=80,
-                                      crossover_independent_proba=0.5,
-                                      mutation_independent_proba=0.05,
-                                      tournament_size=5,
-                                      n_gen_no_change=10,
-                                      caching=True,
-                                      n_jobs=-1)
-    selector = selector.fit(X, y)
-    support_names = [columns[i] for i, s in enumerate(selector.support_) if s]
-    return {
-        # pick selected features names
-        'support': support_names,
-        # pick feature coefficients
-        #'coef': {support_names[i]: c for i, c in enumerate(selector.estimator_.coef_)},
-    }
-
 def randomforest(X, y, columns):
     forest = RandomForestClassifier(n_estimators=250, random_state=0)
     forest.fit(X,y)
@@ -71,7 +24,7 @@ def randomforest(X, y, columns):
 
 def randomforest_rfecv(X, y, columns):
     estimator = RandomForestClassifier(n_estimators=250, random_state=0)
-    selector = RFECV(estimator, step=1, cv=3, verbose=1)
+    selector = RFECV(estimator, step=1, cv=5, verbose=0)
     selector = selector.fit(X, y)
     # selector ranking to column:rank pairs
     rank = {columns[i]: s for i, s in enumerate(selector.ranking_)}
@@ -90,8 +43,8 @@ def randomforest_rfecv(X, y, columns):
 def randomforest_genetic(X, y, columns):
     estimator = RandomForestClassifier(n_estimators=250, random_state=0)
     selector = GeneticSelectionCV(estimator,
-                                      cv=3,
-                                      verbose=1,
+                                      cv=5,
+                                      verbose=0,
                                       scoring="accuracy",
                                       max_features=min(X.shape[1], 30),
                                       n_population=50,
@@ -130,13 +83,12 @@ def main(dataset):
         y = df[['target']]['target'].values
         run = {}
         # Split train and test set in an expanding window fashion (decrease test set)
-        for testSize in [0.5, 0.3, 0.1]:
+        for testSize in [0.3]:
             X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=False, test_size=testSize)
             run["test_size={}".format(testSize)] = {
-                #'rfecv': rfecv_rank(X_train, y_train, X.columns),
                 'genetic': randomforest_genetic(X_train, y_train, X.columns),
-                #'randomforest': randomforest(X_train, y_train, X.columns),
-                'randomforest': randomforest_rfecv(X_train, y_train, X.columns),
+                'rfecv': randomforest_rfecv(X_train, y_train, X.columns),
+                'randomforest': randomforest(X_train, y_train, X.columns)
             }
         result[_sym] = run
 
